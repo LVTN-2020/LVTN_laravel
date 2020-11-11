da01ae9cc1d52e2768e410a8c37230150f90f60a
= <=>
2020-11-11 15:46:35 +0700

    commit to respository

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -40,7 +40,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Admin Area - Khoa Phạm</a>
+                <a class="navbar-brand" href="{{URL::to(/admin)}}">Admin</a>
             </div>
             <!-- /.navbar-header -->
 
@@ -130,42 +130,12 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">User
-                            <small>Add</small>
+                        <h1 class="page-header">@yield('title')
+                            <small>@yield('action')</small>
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
-                    <div class="col-lg-7" style="padding-bottom:120px">
-                        <form action="" method="POST">
-                            <div class="form-group">
-                                <label>Username</label>
-                                <input class="form-control" name="txtUser" placeholder="Please Enter Username" />
-                            </div>
-                            <div class="form-group">
-                                <label>Password</label>
-                                <input type="password" class="form-control" name="txtPass" placeholder="Please Enter Password" />
-                            </div>
-                            <div class="form-group">
-                                <label>RePassword</label>
-                                <input type="password" class="form-control" name="txtRePass" placeholder="Please Enter RePassword" />
-                            </div>
-                            <div class="form-group">
-                                <label>Email</label>
-                                <input type="email" class="form-control" name="txtEmail" placeholder="Please Enter Email" />
-                            </div>
-                            <div class="form-group">
-                                <label>User Level</label>
-                                <label class="radio-inline">
-                                    <input name="rdoLevel" value="1" checked="" type="radio">Admin
-                                </label>
-                                <label class="radio-inline">
-                                    <input name="rdoLevel" value="2" type="radio">Member
-                                </label>
-                            </div>
-                            <button type="submit" class="btn btn-default">User Add</button>
-                            <button type="reset" class="btn btn-default">Reset</button>
-                        <form>
-                    </div>
+                    @yield('content')
                 </div>
                 <!-- /.row -->
             </div>
