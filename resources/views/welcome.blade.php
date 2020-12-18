3d56659bab2c3b3356131fa47b735e16ea88133b
= <=>
2020-12-18 14:15:20 +0700

    fix loi file welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -211,13 +211,13 @@
 						</div>
 					</div>
 					<div class="col-sm-5">
-						<form action="{{URL::to('/tim-kiem')}}" method="POST">
-                            {{csrf_field()}}
+						{{-- <form action="{{URL::to('/tim-kiem')}}" method="POST">
+                            {{csrf_field()}} --}}
                         <div class="search_box pull-right">
-                            <input type="text" name="keywords_submit" placeholder="Tìm kiếm sản phẩm"/>
-                            <input type="submit" style="margin-top:0;color:#666" name="search_items" class="btn btn-primary btn-sm" value="Tìm kiếm">
+                            <input type="text" name="search" id="search" placeholder="Tìm kiếm sản phẩm"/>
+                            {{-- <input type="submit" style="margin-top:0;color:#666" name="search_items" class="btn btn-primary btn-sm" value="Tìm kiếm"> --}}
                         </div>
-                        </form>
+                        {{-- </form> --}}
 						</div>	
 					</div>
 				</div>
@@ -333,54 +333,7 @@
 				</div>
 				
 				<div class="col-sm-9 padding-right">
-					@yield('content')
-					 <div class="recommended_items"><!--recommended_items-->
-						<h2 class="title text-center">recommended items</h2>
-						
-						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
-							<div class="carousel-inner">
-								<div class="item active">	
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="images/home/recommend1.jpg" alt="" />
-													<h2>$56</h2>
-													<p>Easy Polo Black Edition</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-									
-								</div>
-								<div class="item">	
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="images/home/recommend1.jpg" alt="" />
-													<h2>$56</h2>
-													<p>Easy Polo Black Edition</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-									
-								</div>
-							</div>
-							 <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
-								<i class="fa fa-angle-left"></i>
-							  </a>
-							  <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
-								<i class="fa fa-angle-right"></i>
-							  </a>			
-						</div>
-					</div><!--/recommended_items--> 
-					
+					@yield('content')		
 				</div>
 			</div>
 		</div>
